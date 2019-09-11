4089b48a9517a84a47d76132cd821c3690f36e36
ben <ben@benjaminbedard.com>
2019-09-10 20:46:55 -0400

    Update pack-dependencies.sh

--- pack-dependencies.sh
+++ pack-dependencies.sh
@@ -2,17 +2,18 @@
 
 cd deps/Neuralia.Data.HashFunction.xxHash
 
-dotnet restore
-
-if dotnet build -c Release --no-incremental ; then
-    if  dotnet pack -c Release -o ../ ; then
-	dotnet clean ;
-         echo "pack completed"
-    else
-        echo "build failed"
-    fi
-else
-    echo "build failed"
+if test -f "Neuralia.Data.HashFunction.xxHash.1.0.0.nupkg"; then
+	dotnet restore
+	if dotnet build -c Release --no-incremental ; then
+		if  dotnet pack -c Release -o ../ ; then
+		dotnet clean ;
+			 echo "pack completed"
+		else
+			echo "build failed"
+		fi
+	else
+		echo "build failed"
+	fi
 fi
 
 cp Neuralia.Data.HashFunction.xxHash.1.0.0.nupkg ../neuralia.blockchains.tools/nuget-source/
@@ -22,36 +23,38 @@ cp Neuralia.Data.HashFunction.xxHash.1.0.0.nupkg ../Neuralia.BouncyCastle/nuget-
 
 cd ../neuralia.blockchains.tools
 
-dotnet restore
-
-if dotnet build -c Release --no-incremental ; then
-    if  dotnet pack -c Release -o ../ ; then
-	dotnet clean ;
-         echo "pack completed"
-    else
-        echo "build failed"
-    fi
-else
-    echo "build failed"
+if test -f "neuralia.Blockchains.Tools.1.0.0.nupkg"; then
+	dotnet restore
+	if dotnet build -c Release --no-incremental ; then
+		if  dotnet pack -c Release -o ../ ; then
+		dotnet clean ;
+			 echo "pack completed"
+		else
+			echo "build failed"
+		fi
+	else
+		echo "build failed"
+	fi
 fi
 
-cp Neuralia.Blockchains.Tools.1.0.0.nupkg ../Neuralia.Blockchain/nuget-source/
-cp Neuralia.Blockchains.Tools.1.0.0.nupkg ../euralia.BouncyCastle/nuget-source/
-cp Neuralia.Blockchains.Tools.1.0.0.nupkg ../../nuget-source/
+cp neuralia.Blockchains.Tools.1.0.0.nupkg ../Neuralia.Blockchain/nuget-source/
+cp neuralia.Blockchains.Tools.1.0.0.nupkg ../Neuralia.BouncyCastle/nuget-source/
+cp neuralia.Blockchains.Tools.1.0.0.nupkg ../../nuget-source/
 
 cd ../Neuralia.STUN
 
-dotnet restore
-
-if dotnet build -c Release --no-incremental ; then
-    if  dotnet pack -c Release -o ../ ; then
-	dotnet clean ;
-         echo "pack completed"
-    else
-        echo "build failed"
-    fi
-else
-    echo "build failed"
+if test -f "Neuralia.STUN.1.0.0.nupkg"; then
+	dotnet restore
+	if dotnet build -c Release --no-incremental ; then
+		if  dotnet pack -c Release -o ../ ; then
+		dotnet clean ;
+			 echo "pack completed"
+		else
+			echo "build failed"
+		fi
+	else
+		echo "build failed"
+	fi
 fi
 
 cp Neuralia.STUN.1.0.0.nupkg ../Neuralia.Blockchain/nuget-source/
@@ -60,17 +63,18 @@ cp Neuralia.STUN.1.0.0.nupkg ../../nuget-source/
 
 cd ../neuralia.BouncyCastle/nuget-source/
 
-dotnet restore
-
-if dotnet build Neuralia.BouncyCastle.sln -c Release --no-incremental ; then
-    if  dotnet pack Neuralia.BouncyCastle.sln -c Release -o ../ ; then
-	dotnet clean ;
-         echo "pack completed"
-    else
-        echo "build failed"
-    fi
-else
-    echo "build failed"
+if test -f "Neuralia.BouncyCastle.1.0.0.nupkg"; then
+	dotnet restore
+	if dotnet build Neuralia.BouncyCastle.sln -c Release --no-incremental ; then
+		if  dotnet pack Neuralia.BouncyCastle.sln -c Release -o ../ ; then
+		dotnet clean ;
+			 echo "pack completed"
+		else
+			echo "build failed"
+		fi
+	else
+		echo "build failed"
+	fi
 fi
 
 cp Neuralia.BouncyCastle.1.0.0.nupkg ../Neuralia.Blockchain/nuget-source/
@@ -78,18 +82,18 @@ cp Neuralia.BouncyCastle.1.0.0.nupkg ../../nuget-source/
 
 cd ../neuralia.Blockchain/nuget-source/
 
-
-dotnet restore
-
-if dotnet build -c Release --no-incremental ; then
-    if  dotnet pack -c Release -o ../../ ; then
-	dotnet clean ;
-         echo "pack completed"
-    else
-        echo "build failed"
-    fi
-else
-    echo "build failed"
+if [test -f "Neuralia.Blockchains.Common.1.0.0.nupkg"] & [test -f "Neuralia.Blockchains.Core.1.0.0.nupkg"]; then
+	dotnet restore
+	if dotnet build -c Release --no-incremental ; then
+		if  dotnet pack -c Release -o ../../ ; then
+		dotnet clean ;
+			 echo "pack completed"
+		else
+			echo "build failed"
+		fi
+	else
+		echo "build failed"
+	fi
 fi
 
 cp Neuralia.Blockchains.Common.1.0.0.nupkg ../../nuget-source/
